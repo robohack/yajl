32bff475715e7352fcf956ad8c9d2eec47fedb7f
Greg A. Woods <woods@planix.com>
2019-03-30 19:46:07 -0700

    appease Clang with a cast

--- src/yajl_parser.c
+++ src/yajl_parser.c
@@ -357,7 +357,7 @@ yajl_do_parse(yajl_handle hand, const unsigned char * jsonText,
             }
             /* got a value.  transition depends on the state we're in. */
             {
-                yajl_state s = yajl_bs_current(hand->stateStack);
+                yajl_state s = (yajl_state) yajl_bs_current(hand->stateStack);
                 if (s == yajl_state_start || s == yajl_state_got_value) {
                     yajl_bs_set(hand->stateStack, yajl_state_parse_complete);
                 } else if (s == yajl_state_map_need_val) {
